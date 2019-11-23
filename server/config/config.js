cefdeb72f6bb395dd70926f2e9b65bfddcef3849
Lukasz Lach <mietek76@gmail.com>
2019-11-23 13:43:42 +0100

    Add config files

---
+++ server/config/config.js
@@ -0,0 +1,31 @@
+const config = {
+    development: {
+        httpPort: 3000,
+        httpsPort: 3001,
+        envName: 'development',
+        mongoDbUrl: process.env.DB_URL,
+        cryptoSecret: process.env.DB_SECRET,
+        sessionConfig: {
+            secret: process.env.SESSION_SECRET,
+        },
+        cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+        cloudinaryApiSecret: process.env.CLOUDINARY_SECRET,
+    },
+    production: {
+        httpPort: 80,
+        httpsPort: 443,
+        envName: 'production',
+        mongoDbUrl: process.env.DB_URL,
+        cryptoSecret: process.env.DB_SECRET,
+        sessionConfig: {
+            secret: process.env.SESSION_SECRET,
+        },
+        cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+        cloudinaryApiSecret: process.env.CLOUDINARY_SECRET,
+    }
+};
+const env = process.env.NODE_ENV;
+
+module.exports = config[env] || config.development;
